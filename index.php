0efc0bb4ff9d7420b02f60e98660d2bf48cba504
Olga Marinina <youarewonderful@yandex.ru>
2022-05-17 21:41:26 +0700

    Поправила вывод категорий и массивы

--- index.php
+++ index.php
@@ -2,42 +2,50 @@
 
 $is_auth = rand(0, 1);
 $user_name = 'Olga'; // укажите здесь ваше имя
-$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+$categories = [
+    'boards' =>'Доски и лыжи',
+    'attachment' => 'Крепления',
+    'boots' => 'Ботинки',
+    'clothing' => 'Одежда',
+    'tools' => 'Инструменты',
+    'other' => 'Разное'
+];
+
 $goods = [
     [
         'name' => '2014 Rossignol District Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => '10999',
+        'category' => $categories['boards'],
+        'price' => 10999,
         'url_image' => 'img/lot-1.jpg'
     ],
     [
         'name' => 'DC Ply Mens 2016/2017 Snowboard',
-        'category' => 'Доски и лыжи',
-        'price' => '159999',
+        'category' => $categories['boards'],
+        'price' => 159999,
         'url_image' => 'img/lot-2.jpg'
     ],
     [
         'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'category' => 'Крепления',
-        'price' => '8000',
+        'category' => $categories['attachment'],
+        'price' => 8000,
         'url_image' => 'img/lot-3.jpg'
     ],
     [
         'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'category' => 'Ботинки',
-        'price' => '10999',
+        'category' => $categories['boots'],
+        'price' => 10999,
         'url_image' => 'img/lot-4.jpg'
     ],
     [
         'name' => 'Куртка для сноуборда DC Mutiny Charocal',
-        'category' => 'Одежда',
-        'price' => '7500',
+        'category' => $categories['clothing'],
+        'price' => 7500,
         'url_image' => 'img/lot-5.jpg'
     ],
     [
         'name' => 'Маска Oakley Canopy',
-        'category' => 'Разное',
-        'price' => '5400',
+        'category' => $categories['other'],
+        'price' => 5400,
         'url_image' => 'img/lot-6.jpg'
     ]
 ]
@@ -93,8 +101,8 @@ $goods = [
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <?php foreach ($categories as $category) : ?>
-            <li class="promo__item promo__item--boards">
+            <?php foreach ($categories as $key => $category) : ?>
+            <li class="promo__item promo__item--<?= $key ?>">
                 <a class="promo__link" href="pages/all-lots.html"><?= $category ?></a>
             </li>
             <?php endforeach; ?>
