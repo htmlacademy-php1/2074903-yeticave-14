0fb22704888ad8773863e7190a6800150ba5f76b
Olga Marinina <youarewonderful@yandex.ru>
2022-05-25 20:36:50 +0700

    Установила соединение с БД

--- init.php
+++ init.php
@@ -2,3 +2,12 @@
 
 require_once 'config/db.php';
 date_default_timezone_set('Europe/Moscow');
+
+$connect = mysqli_connect($db['host'], $db['user'], $db['password'], $db['database']);
+mysqli_set_charset($connect, 'utf8');
+
+if (!$connect) {
+    $dbError = mysqli_connect_error();
+    $content = 'Ошибка подключения: ' . $dbError;
+};
+
